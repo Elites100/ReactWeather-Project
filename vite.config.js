87ee46619a5420e975e659459e79bdaf492bae00
Elites100 <105951171+Elites100@users.noreply.github.com>
2024-09-02 14:58:52 -0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: 'ReactWeather-Project',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/ReactWeather-Project/' : '/',
   plugins: [react()],
   server:{
     port: 3000,
   }
-})
+}));
